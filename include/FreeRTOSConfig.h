3b6aa58d313387445499148687c8be609bfbf7cb
mehdi-norouzi <mehdeenoroozi@gmail.com>
2023-10-29 10:29:59 +0330

    chore: Activate idle hook and enable critical nesting

--- include/FreeRTOSConfig.h
+++ include/FreeRTOSConfig.h
@@ -47,7 +47,8 @@
 #endif
 
 #define configUSE_PREEMPTION			1
-#define configUSE_IDLE_HOOK				0
+#define configUSE_IDLE_HOOK				1
+#define portCRITICAL_NESTING_IN_TCB     1
 #define configUSE_TICK_HOOK				0
 #define configCPU_CLOCK_HZ				( 168000000 )
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
